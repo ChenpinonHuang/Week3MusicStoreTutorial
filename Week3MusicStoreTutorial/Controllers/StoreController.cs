fb63d2643cb0da13703c184e7e035f757be12099
ChenpinonHuang <ChenpinonHuang@users.noreply.github.com>
2019-09-16 18:15:12 -0400

    Add project files.

---
+++ Week3MusicStoreTutorial/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week3MusicStoreTutorial.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World from Store.Index()");
+        }
+
+        public IActionResult Browse(string genre)
+        {
+            genre = HttpUtility.HtmlEncode("Store.Browse, genre = " + genre);
+
+            return Ok(genre);
+            //return Ok("Result = " + (x * y));
+
+        }
+        public IActionResult Details(int id)
+        {
+
+            return Ok(id.ToString());
+        }
+
+
+    }
+}
